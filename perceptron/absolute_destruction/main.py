f5f90c55a43b3db382899fc8bab79c3cf68a1e14
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-21 16:22:56 -0300

    Trying to fix a error

--- perceptron/absolute_destruction/main.py
+++ perceptron/absolute_destruction/main.py
@@ -1,7 +1,6 @@
 import random
 weights = []
 inputs = [10, 50]
-the_sum = 0
 
 for i in range(7):
     weights.append(random.randint(-1000,1000))
@@ -10,17 +9,18 @@ def jump():
     print("jumped")
 
 def sum_function():
+    global the_sum = 0
     for weight in weights:
-            the_sum += weight * inputs[1]
-            the_sum += weight * inputs[2]
+            the_sum += (weight * inputs[0])
+            the_sum += (weight * inputs[1])
     
 def step_function():
     if the_sum > 0:
         jump()
 
-while true:
+while True:
+    inputs[0] += 1
     inputs[1] += 1
-    inputs[2] += 1
     sum_function()
     step_function()
     time.sleep(0.1)
