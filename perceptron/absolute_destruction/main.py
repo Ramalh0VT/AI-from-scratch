9e76d48f4daa0836eb2fb8fdba7ee7f2ea13c1fb
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-21 16:18:21 -0300

    Error fix: functions were missing parentheses

--- perceptron/absolute_destruction/main.py
+++ perceptron/absolute_destruction/main.py
@@ -6,19 +6,25 @@ the_sum = 0
 for i in range(7):
     weights.append(random.randint(-1000,1000))
 
-def jump:
+def jump():
     print("jumped")
 
-def sum_function:
+def sum_function():
     for weight in weights:
             the_sum += weight * inputs[1]
             the_sum += weight * inputs[2]
     
-def step_function:
+def step_function():
     if the_sum > 0:
         jump()
 
 while true:
+    inputs[1] += 1
+    inputs[2] += 1
+    sum_function()
+    step_function()
+    time.sleep(0.1)
+
     
 
     
