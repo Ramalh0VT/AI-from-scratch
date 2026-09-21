823821b18787468ba70f027af8ecf3244e36a68c
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-21 16:16:18 -0300

    Feat: updated the system

--- perceptron/absolute_destruction/main.py
+++ perceptron/absolute_destruction/main.py
@@ -1,8 +1,24 @@
 import random
 weights = []
+inputs = [10, 50]
+the_sum = 0
 
 for i in range(7):
     weights.append(random.randint(-1000,1000))
 
-print(weights)
+def jump:
+    print("jumped")
+
+def sum_function:
+    for weight in weights:
+            the_sum += weight * inputs[1]
+            the_sum += weight * inputs[2]
+    
+def step_function:
+    if the_sum > 0:
+        jump()
+
+while true:
+    
+
     
