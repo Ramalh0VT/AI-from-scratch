46d0c78166a680241e2bb0583eb72c92911273a1
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-22 15:03:07 -0300

    Deleted unused content

--- perceptron/absolute_destruction/main.py
+++ perceptron/absolute_destruction/main.py
@@ -5,6 +5,7 @@ import pygame
 class ai:
     def __init__(self):
         self.weights = []
+        self.pos = pygame.Vector2(0,0)
         for i in range(7):
             self.weights.append(random.randint(-1000,1000))
 
@@ -31,7 +32,6 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-initial_pos = pygame.Vector2(0,0)
 
 while running:
     for event in pygame.event.get():
@@ -45,39 +45,4 @@ pygame.quit()
 
 
 
-"""
-weights = []
-inputs = [0, 0]
-
-for i in range(7):
-    weights.append(random.randint(-1000,1000))
-
-def jump():
-    print("jumped")
-
-def sum_function():
-    the_sum = 0
-    for weight in weights:
-            the_sum += (weight * inputs[0])
-            the_sum += (weight * inputs[1])
-    if the_sum > 0:
-        step_function()
-    else:
-        print("didn't jump")
-    print(f'sum: ${the_sum}')
-    
-    
-def step_function():
-        jump()
-
-print(weights)
-print(inputs)
-
-while True:
-    inputs[0] += 1
-    inputs[1] += 1
-    print(inputs)
-    sum_function()
-    time.sleep(1)
-"""
 
